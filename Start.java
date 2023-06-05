9017a75b14ebcd8f30b2d690c777cc88ba878ee0
FrancescoSko <francescobuonora11@gmail.com>
2023-06-06 00:13:19 +0200

    That's the first exercise

---
+++ Start.java
@@ -0,0 +1,9 @@
+package co.develhope.introduction;
+
+class Main {
+public static void main(String [] args){
+    String myBirthday = "20 November";
+    System.out.println("Hello develhope java student "+ myBirthday);
+
+}
+}
