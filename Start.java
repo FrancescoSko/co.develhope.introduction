45d7a3e3b372446645dcc9b9b70e164cf3ea1973
FrancescoSko <133954449+FrancescoSko@users.noreply.github.com>
2023-06-06 01:09:37 +0200

    Delete Start.java

--- Start.java
+++
@@ -1,9 +0,0 @@
-package co.develhope.introduction;
-
-class Main {
-public static void main(String [] args){
-    String myBirthday = "20 November";
-    System.out.println("Hello develhope java student "+ myBirthday);
-
-}
-}
