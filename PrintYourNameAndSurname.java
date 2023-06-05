24f8d84dd2f165d9d30da65f8a107c72be2eb07b
FrancescoSko <francescobuonora11@gmail.com>
2023-06-06 01:22:51 +0200

    This is the third exercise

---
+++ PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package co.develhope.introduction;
+
+public class PrintYourNameAndSurname {
+    public static void main (String[] args){
+        System.out.print("Francesco ");
+        System.out.println("Buonora");
+    }
+}
