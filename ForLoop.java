a73cbbc990a931d7dd092119b8226c520f542090
FrancescoSko <francescobuonora11@gmail.com>
2023-06-13 22:52:34 +0200

    That's the 12th exercise and is about the for loop

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+package co.develhope.introduction;
+
+public class ForLoop {
+    public static void main(String[] args) {
+     for (int i = 5; i <= 25; i++){
+         System.out.println(i);
+     }
+    }
+}
+
+
+
+
+
+
+
+
+
+
