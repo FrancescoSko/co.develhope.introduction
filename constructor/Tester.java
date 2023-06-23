18db1f460732c814c49ce76502a73856ece5f3d3
FrancescoSko <francescobuonora11@gmail.com>
2023-06-24 00:19:44 +0200

    That's the 16th exercise and is about constructor

---
+++ constructor/Tester.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction.constructor;
+
+public class Tester {
+    public static void main (String [] args) {
+
+        House house1 = new House("Via Garibaldi, 1", 4, 11);
+        House house2 = new House("Piazza Vittorio Veneto, 12", 7, 32);
+
+    }
+}
